53a26e78bc89af581e1c8c4a98712524dc8520f3
steve tonneau <stevetonneau@hotmail.fr>
2013-12-21 16:47:36 +0100

    porte spline en c sharp

---
+++ unity/Assets/editor/SplineEditor.cs
@@ -0,0 +1,24 @@
+﻿// MyScriptEditor.cs
+using UnityEditor;
+using UnityEngine;
+using System.Collections;
+
+//[CustomEditor(typeof(Spline))]
+public class SplineEditor : Editor {
+	
+	SerializedProperty valueProperty;
+	
+	void OnEnable() {
+		// Setup serialized property
+		valueProperty = serializedObject.FindProperty("tg");
+	}
+	
+	public override void OnInspectorGUI() {
+		// Update the serializedProperty
+		// always do this at the start of OnInspectorGUI
+		serializedObject.Update();
+		EditorGUILayout.IntSlider(valueProperty, 1, 10, new GUIContent("Tg"));
+		serializedObject.ApplyModifiedProperties();
+	}
+	
+}
